400d1980c417335f21af5e1efe9ef47b1d40885a
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2024-04-26 13:46:49 +0000

    A realy good session

--- Js/Main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("product").addEventListener("change", function() {
-  var selectedOption = this.value;
-  if (selectedOption) {
-    window.location.href = selectedOption; // Navigate to the selected URL
-    // Alternatively, you can call a JavaScript function based on the selected option:
-    // handleOption(selectedOption);
-  }
-});
