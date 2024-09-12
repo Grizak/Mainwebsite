06cdade4441351101a3ffc0034c8e474fa93ee4e
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2024-09-12 15:46:32 +0000

    gsdof

---
+++ Web/JS/Main.js
@@ -0,0 +1,24 @@
+function loadHeader() {
+  fetch('/Web/HTML/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
+}
+
+function loadFooter() {
+  fetch('/Web/HTML/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
+}
+
+function wO () {
+  loadFooter()
+  loadHeader()
+}
+
+window.onload = wO();
